26d0f48c912091ce3c5a4532fb7c3ef29548e52c
Lukasz Linux <lukasz@broll.pl>
2017-12-15 01:37:39 +0100

    Test private def

--- testKlasy.py
+++ testKlasy.py
@@ -1,12 +1,20 @@
 class Facade:
 	__prvAttribute="print __prvAttribute"
+	__prvToPrvDef="print __prvToPrvDef"
 	pubAttribute = "print pubAttribute"
+
 	def getPrvByPublicDef(self):
 		print self.__prvAttribute
+	def __prvDef__(self):
+		return __prvToPrvDef
+	def PrvToPubReturn(self):
+		__prvDef__()
 def test():
 	obj = Facade()
 	obj.getPrvByPublicDef()
 	print obj.pubAttribute
+	
+	obj.PrvToPubReturn()
 	del obj
 test()
 
