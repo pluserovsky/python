ccab18ff3d7bc4ce8d020a3bc5ff310ef0a00ce5
Lukasz Linux <lukasz@broll.pl>
2017-12-15 13:19:49 +0100

    dziedziczenie

--- testKlasy.py
+++ testKlasy.py
@@ -2,19 +2,28 @@ class Facade:
 	__prvAttribute="print __prvAttribute"
 	__prvToPrvDef="print __prvToPrvDef"
 	pubAttribute = "print pubAttribute"
-
+	goPub="goPub1"
 	def getPrvByPublicDef(self):
 		print self.__prvAttribute
+		goPub="goPub"
+		print goPub
 	def __prvDef__(self):
-		return __prvToPrvDef
+#		self.goPub=self.__prvToPrvDef
+		return self.goPub
 	def PrvToPubReturn(self):
-		__prvDef__()
+		self.__prvDef__()
+class Params(Facade):
+	def printFacadeAttrbs(self):
+		print Facade.pubAttribute
+		print Facade.goPub
 def test():
-	obj = Facade()
-	obj.getPrvByPublicDef()
-	print obj.pubAttribute
-	
-	obj.PrvToPubReturn()
-	del obj
+#	obj = Facade()
+#	obj.getPrvByPublicDef() goPub
+#	print obj.PrvToPubReturn() # None
+#	print obj.pubAttribute
+#	del obj
+	par = Params()
+	par.printFacadeAttrbs()
+	del par
 test()
 
